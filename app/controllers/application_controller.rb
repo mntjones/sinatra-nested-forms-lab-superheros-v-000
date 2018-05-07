38b376b782b02aebdc0135c60734f5c4427dfc06
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-07 00:23:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,7 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params[:team])
  
-      h = params[:team][:members].values
-      h.each do |details|
+      params[:team][:members].each do |details|
         Hero.new(details)
       end
 
